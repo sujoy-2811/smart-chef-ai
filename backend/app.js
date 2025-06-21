d49d794d46d4eb141a430df5cbf29aaf38420a5f
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-06-21 10:15:00 +0530

    initial setup for backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 
+// middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.send("API running"));
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}...`);
+  console.log(`Server running on ${PORT}....`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
 });
